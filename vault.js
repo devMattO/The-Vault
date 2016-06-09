f18f49a87ac448c4023607a158a2abb9d3f99eaf
Mattdre <olsen_matthew@yahoo.com>
2016-06-08 19:54:29 -1000

    wrote both functions && they work!

--- vault.js
+++ vault.js
@@ -1,25 +1,21 @@
 'use strict';
 module.exports = function() {
 
-var storeKey = {};
+  var storeKey = {};
 
   function _setValue( theKey, theValue ){
-    //console.log(theKey, theValue);
     storeKey[theKey] = theValue;
-    //console.log(storeKey);
   }
 
   function _getValue( theKey ){
-    if (theKey === undefined || storeKey[theKey] === undefined) {
+    if( theKey === undefined || storeKey[theKey] === undefined) {
       return null;
     }else{
-      console.log('hello', storeKey[theKey]);
       return storeKey[theKey];
     }
-
   }
-return {
-  setValue: _setValue,
-  getValue: _getValue
+  return {
+    setValue: _setValue,
+    getValue: _getValue
 };
 };
