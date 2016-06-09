bcaa44c0a28ce6eb75ca22a5280bd77333bbd666
Mattdre <olsen_matthew@yahoo.com>
2016-06-08 19:03:43 -1000

    wrote both functions, debugging ATM

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+
+var storeKey;
+
+  function setValue( key, value){
+    value = key;
+    storeKey = value;
+  }
+
+  function getValue( key ){
+    if (key === undefined) {
+      return null;
+    }else{
+      return storeKey;
+    }
+
+  }
+
 };
